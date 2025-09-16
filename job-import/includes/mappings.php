01970669059a6fe009baf03bded6aa738825d433
DGC-GH <dimitri@gulla.consulting>
2025-09-16 12:32:00 +0200

    Update mappings.php

--- job-import/includes/mappings.php
+++ job-import/includes/mappings.php
@@ -143,3 +143,112 @@ function get_icon_map() {
         'Transport, Logistique & Achat' => 'fa-shipping-fast',
     ];
 }
+
+if (!function_exists('get_acf_fields')) {
+    function get_acf_fields() {
+        return [
+            'guid', 'category', 'title', 'description', 'pubdate', 'updated', 'link', 'applylink', 'magiclink',
+            'branche', 'postalcode', 'city', 'province', 'provincecode', 'country', 'validfrom', 'validtill',
+            'channeltype', 'functiongroup', 'functiongroup2', 'functiongroup3', 'functiongroupid',
+            'functiongroupid2', 'functiongroupid3', 'function', 'function2', 'function3', 'functionid',
+            'functionid2', 'functionid3', 'functiontitle', 'functiondescription', 'education', 'education2',
+            'education3', 'educationid', 'educationid2', 'educationid3', 'educationgroup', 'educationgroup2',
+            'educationgroup3', 'educationgroupcode', 'educationgroupcode2', 'educationgroupcode3', 'jobtype',
+            'jobtypecode', 'jobtypegroup', 'jobtypegroupcode', 'contracttype', 'contracttype2', 'contracttype3',
+            'contracttypecode', 'contracttypecode2', 'contracttypecode3', 'experience', 'experiencecode', 'brand',
+            'accountid', 'internal', 'payrollid', 'payroll', 'brancheid', 'label', 'labelid', 'language',
+            'language2', 'language3', 'languagecode', 'languagecode2', 'languagecode3', 'languagelevel',
+            'languagelevel2', 'languagelevel3', 'languagelevelcode', 'languagelevelcode2', 'languagelevelcode3',
+            'office', 'officeid', 'officestreet', 'officehousenumber', 'officeaddition', 'officepostalcode',
+            'officecity', 'officetelephone', 'officeemail', 'hours', 'salaryfrom', 'salaryto', 'salarytype',
+            'salarytypecode', 'parttime', 'offerdescription', 'requirementsdescription', 'reference', 'shift',
+            'shiftcode', 'driverslicense', 'driverslicenseid', 'publicationlanguage', 'companydescription',
+            // New fields
+            'job_posting', 'job_icon', 'job_title', 'job_slug', 'job_link', 'job_salary', 'job_apply', 'job_car', 'job_time', 'job_description', 'job_remote', 'job_meal_vouchers', 'job_flex_hours', 'job_skills', 'job_ecommerce', 'job_languages',
+        ];
+    }
+}
+
+if (!function_exists('get_zero_empty_fields')) {
+    function get_zero_empty_fields() {
+        return [
+            'functiongroup2', 'functiongroup3', 'functiongroupid2', 'functiongroupid3', 'function2', 'function3',
+            'functionid2', 'functionid3', 'education2', 'education3', 'educationid2', 'educationid3',
+            'educationgroup2', 'educationgroup3', 'educationgroupcode2', 'educationgroupcode3',
+            'contracttype2', 'contracttype3', 'contracttypecode2', 'contracttypecode3',
+            'language2', 'language3', 'languagecode2', 'languagecode3', 'languagelevel2', 'languagelevel3',
+            'languagelevelcode2', 'languagelevelcode3', 'salaryfrom', 'salaryto',
+        ];
+    }
+}
+
+if (!function_exists('build_job_schema')) {
+    function build_job_schema($enhanced_title, $job_desc, $item, $norm_province, $job_time, $job_remote, $fg, $estimate_key) {
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'JobPosting',
+            'title' => $enhanced_title,
+            'description' => $job_desc,
+            'datePosted' => isset($item->pubdate) ? date('c', strtotime((string)$item->pubdate)) : date('c'),
+            'validThrough' => isset($item->validtill) ? date('c', strtotime((string)$item->validtill)) : null,
+            'hiringOrganization' => [
+                '@type' => 'Organization',
+                'name' => isset($item->companydescription) ? strip_tags((string)$item->companydescription) : 'Unknown',
+            ],
+            'jobLocation' => [
+                '@type' => 'Place',
+                'address' => [
+                    '@type' => 'PostalAddress',
+                    'addressLocality' => isset($item->city) ? (string)$item->city : '',
+                    'addressRegion' => isset($item->province) ? (string)$item->province : '',
+                    'postalCode' => isset($item->postalcode) ? (string)$item->postalcode : '',
+                    'addressCountry' => 'BE',
+                ]
+            ],
+            'employmentType' => $job_time,
+            'jobLocationType' => $job_remote ? 'TELECOMMUTE' : null,
+            'applicantLocationRequirements' => $job_remote ? [
+                '@type' => 'Country',
+                'name' => 'Belgium',
+            ] : null,
+            'occupationalCategory' => $fg,
+            'baseSalary' => $estimate_key ? [
+                '@type' => 'MonetaryAmount',
+                'currency' => 'EUR',
+                'value' => [
+                    '@type' => 'QuantitativeValue',
+                    'minValue' => get_salary_estimates()[$estimate_key]['low'],
+                    'maxValue' => get_salary_estimates()[$estimate_key]['high'],
+                    'unitText' => 'MONTH',
+                ]
+            ] : null,
+            'url' => isset($item->job_link) ? $item->job_link : 'https://' . $norm_province,
+            'identifier' => [
+                '@type' => 'PropertyValue',
+                'name' => 'GUID',
+                'value' => (string)$item->guid,
+            ],
+        ];
+        return $schema;
+    }
+}
+
+if (!function_exists('build_ecomm_schema')) {
+    function build_ecomm_schema($enhanced_title, $job_desc, $item, $estimate_key) {
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'Product',
+            'name' => $enhanced_title,
+            'description' => $job_desc,
+            'category' => isset($item->functiongroup) ? (string)$item->functiongroup : '',
+            'offers' => $estimate_key ? [
+                '@type' => 'Offer',
+                'priceCurrency' => 'EUR',
+                'price' => (get_salary_estimates()[$estimate_key]['low'] + get_salary_estimates()[$estimate_key]['high']) / 2,
+                'availability' => 'https://schema.org/InStock',
+                'url' => isset($item->applylink) ? (string)$item->applylink : '',
+            ] : null,
+        ];
+        return $schema;
+    }
+}
