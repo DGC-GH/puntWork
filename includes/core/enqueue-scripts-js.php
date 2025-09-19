c08676c22b340807164572eaa0be9fe5475ecb47
DGC-GH <dimitri@gulla.consulting>
2025-09-19 13:27:53 +0200

    fix

--- includes/core/enqueue-scripts-js.php
+++ includes/core/enqueue-scripts-js.php
@@ -145,18 +145,9 @@ function enqueue_job_import_scripts() {
         );
 
         // Localize script with data
-        $feeds = [];
-        try {
-            $feeds = array_keys(get_feeds());
-            error_log('Job Import: Found ' . count($feeds) . ' feeds: ' . implode(', ', $feeds));
-        } catch (Exception $e) {
-            error_log('Error getting feeds for JavaScript: ' . $e->getMessage());
-            $feeds = [];
-        }
-
         wp_localize_script('job-import-admin-js', 'jobImportData', [
             'nonce' => wp_create_nonce('job_import_nonce'),
-            'feeds' => $feeds,
+            'feeds' => [],
             'ajaxurl' => admin_url('admin-ajax.php'),
             'resume_progress' => (int) get_option('job_import_progress', 0)
         ]);
