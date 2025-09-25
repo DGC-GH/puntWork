0dcf9c3be30410cfa26205b4bee6f3ea7887e6f9
DGC-GH <dimitri@gulla.consulting>
2025-09-25 23:27:07 +0200

    Increase maximum batch size from 50 to 500
    - Update all batch size limits in batch-size-management.php to allow up to 500 items per batch
    - Update global batch limit variable in puntwork.php for consistency
    - This allows for much larger batch sizes when memory and performance conditions are favorable

--- puntwork.php
+++ puntwork.php
@@ -86,7 +86,7 @@ add_action( 'init', __NAMESPACE__ . '\\setup_job_import' );
 function setup_job_import() {
     // Global batch limit (from old 1)
     global $job_import_batch_limit;
-    $job_import_batch_limit = 50;
+    $job_import_batch_limit = 500;
     
     // Load includes
     $includes = array(
