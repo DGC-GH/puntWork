db7ba71ad73f8dafa8b5286c352ad291432b23ff
DGC-GH <dimitri@gulla.consulting>
2025-09-26 04:45:12 +0200

    Change sync response message to test if changes are taking effect

--- includes/api/rest-api.php
+++ includes/api/rest-api.php
@@ -240,7 +240,7 @@ function handle_trigger_import($request) {
 
                 return new \WP_REST_Response([
                     'success' => true,
-                    'message' => 'Import completed successfully',
+                    'message' => 'Import completed successfully - SYNC MODE',
                     'data' => $result,
                     'debug' => $debug_info,
                     'async' => false
