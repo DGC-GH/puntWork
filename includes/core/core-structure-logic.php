bb628f73f20a109c93839cfa8bf7f5d6bcc57351
DGC-GH <dimitri@gulla.consulting>
2025-09-30 15:00:26 +0200

    Remove unused PuntWorkLogger import and update protocol data

--- includes/core/core-structure-logic.php
+++ includes/core/core-structure-logic.php
@@ -14,7 +14,6 @@ if (!defined('ABSPATH')) {
 }
 
 use Puntwork\Utilities\CacheManager;
-use Puntwork\PuntWorkLogger;
 
 // Include required utility classes
 require_once __DIR__ . '/../utilities/PuntWorkLogger.php';
