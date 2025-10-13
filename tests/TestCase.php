7ff9f5c331e0c66175d5991206b9ad855841d1e8
DGC-GH <dimitri@gulla.consulting>
2025-10-14 00:50:58 +0200

    Bump version to 1.0.9 in plugin and test case for consistency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,7 +28,7 @@ class TestCase extends WP_UnitTestCase {
 
         // Set up test constants if needed
         if (!defined('PUNTWORK_VERSION')) {
-            define('PUNTWORK_VERSION', '1.0.8');
+            define('PUNTWORK_VERSION', '1.0.9');
         }
         if (!defined('PUNTWORK_PATH')) {
             define('PUNTWORK_PATH', dirname(dirname(__DIR__)) . '/');
